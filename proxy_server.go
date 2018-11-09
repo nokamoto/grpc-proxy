6e293dee1f8e60e1467a167c9c4ac4cd304ef249
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2018-11-09 19:41:06 +0900

    Add client-side stream proxy

--- proxy_server.go
+++ proxy_server.go
@@ -7,7 +7,7 @@ import (
 
 type proxyServer interface {
 	unary(context.Context, *message, string) (*message, error)
-	streamC(proxyStreamCServer) error
+	streamC(proxyStreamCServer, string) error
 	streamS(proxyStreamSServer) error
 	streamB(grpc.ServerStream) error
 }
