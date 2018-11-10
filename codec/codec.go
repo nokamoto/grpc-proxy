b8a7d28da490fc8cdae86fd8309a1fa3f5d88b8d
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2018-11-10 14:36:45 +0900

    lint

--- codec/codec.go
+++ codec/codec.go
@@ -1,11 +1,14 @@
 package codec
 
+// RawCodec implements grpc.CustomCodec for RawMessage.
 type RawCodec struct{}
 
+// Marshal returns bytes from RawMessage.
 func (RawCodec) Marshal(v interface{}) ([]byte, error) {
 	return v.(*RawMessage).bytes, nil
 }
 
+// Unmarshal returns RawMessage from the bytes.
 func (RawCodec) Unmarshal(data []byte, v interface{}) error {
 	v.(*RawMessage).bytes = data
 	return nil
