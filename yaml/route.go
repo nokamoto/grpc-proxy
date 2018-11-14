dcd331c7fd87354707d727c3172cd8a3fe690e94
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2018-11-15 08:26:55 +0900

    Add routes.method.equal

--- yaml/route.go
+++ yaml/route.go
@@ -1,9 +1,14 @@
 package yaml
 
+import (
+	"fmt"
+)
+
 // Route represents a configuration of a single gRPC routing roule.
 type Route struct {
 	Method struct {
-		Prefix string
+		Prefix *string `yaml:",omitempty"`
+		Equal  *string `yaml:",omitempty"`
 	}
 	Cluster struct {
 		Name string
@@ -16,4 +21,14 @@ type Route struct {
 			Name *string
 		}
 	}
+}
+
+func (r Route) validate() error {
+	if r.Method.Equal == nil && r.Method.Prefix == nil {
+		return fmt.Errorf("routes.method: equal or prefix must be defined")
+	}
+	if r.Method.Equal != nil && r.Method.Prefix != nil {
+		return fmt.Errorf("routes.method: equal and prefix must not be defined: %s %s", *r.Method.Equal, *r.Method.Prefix)
+	}
+	return nil
 }
