5061a155f283f9373de90d571bcb4334f01dc9ab
Naofumi Okamoto <nokamoto.engr@gmail.com>
2018-11-14 10:16:14 +0900

    Make observe configurations optional (#28)

--- route/route.go
+++ route/route.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nokamoto/grpc-proxy/server"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"os"
 	"time"
@@ -15,8 +16,23 @@ import (
 
 type route struct {
 	cluster cluster.Cluster
-	log     observe.Log
-	prom    observe.Prom
+	log     *observe.Log
+	prom    *observe.Prom
+}
+
+func (r *route) observe(method string, code codes.Code, req, res int, d time.Duration) {
+	if r.log != nil {
+		_, e := (*r.log).Write(method, code, req, res, d)
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "%s: access log error: %s\n", method, e)
+		}
+	}
+	if r.prom != nil {
+		e := (*r.prom).Observe(method, code, req, res, d)
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "%s: unary prom: %s\n", method, e)
+		}
+	}
 }
 
 func (r *route) unary(ctx context.Context, m *codec.RawMessage, method string) (*codec.RawMessage, error) {
@@ -28,15 +44,7 @@ func (r *route) unary(ctx context.Context, m *codec.RawMessage, method string) (
 
 	code := status.Code(err)
 
-	_, e := r.log.Write(method, code, m.Size(), res.Size(), d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: unary access log error: %s\n", method, e)
-	}
-
-	e = r.prom.Observe(method, code, m.Size(), res.Size(), d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: unary prom: %s\n", method, e)
-	}
+	r.observe(method, code, m.Size(), res.Size(), d)
 
 	return res, err
 }
@@ -50,15 +58,7 @@ func (r *route) streamC(stream server.RawServerStreamC, desc *grpc.StreamDesc, m
 
 	code := status.Code(err)
 
-	_, e := r.log.Write(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream c access log error: %s\n", method, e)
-	}
-
-	e = r.prom.Observe(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream c prom: %s\n", method, e)
-	}
+	r.observe(method, code, -1, -1, d)
 
 	return err
 }
@@ -72,15 +72,7 @@ func (r *route) streamS(stream server.RawServerStreamS, desc *grpc.StreamDesc, m
 
 	code := status.Code(err)
 
-	_, e := r.log.Write(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream s access log error: %s\n", method, e)
-	}
-
-	e = r.prom.Observe(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream s prom: %s\n", method, e)
-	}
+	r.observe(method, code, -1, -1, d)
 
 	return err
 }
@@ -94,19 +86,13 @@ func (r *route) streamB(stream server.RawServerStreamB, desc *grpc.StreamDesc, m
 
 	code := status.Code(err)
 
-	_, e := r.log.Write(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream b access log error: %s\n", method, e)
-	}
-
-	e = r.prom.Observe(method, code, -1, -1, d)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%s: stream b prom: %s\n", method, e)
-	}
+	r.observe(method, code, -1, -1, d)
 
 	return err
 }
 
 func (r *route) destroy() {
-	r.prom.Destroy()
+	if r.prom != nil {
+		(*r.prom).Destroy()
+	}
 }
